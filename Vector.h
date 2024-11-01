92fbf15d7a0ada3484627b4b638beb96719da960
AlKurpiakov <s23380350@unn.ru>
2024-11-01 15:28:09 +0300

    aa

--- Vector.h
+++ Vector.h
@@ -18,7 +18,7 @@ public:
     for(int i = 0; i < tmp._size; i++)
         _array[i] = 0;
     
-    };
+    }
 
     Vector(const Vector& tmp){
         _size = tmp._size;
@@ -27,55 +27,55 @@ public:
         for(int i = 0; i < tmp._size; i++){
             _array[i] = tmp._array[i] ;
         }
-    };
+    }
 
     Vector(Vector&& tmp){
     
-    if (!(this == &tmp))
-    {
-        delete[] _array;
-    
-        _array = tmp._array;
-        _size = tmp._size;
+        if (!(this == &tmp))
+        {
+            delete[] _array;
+        
+            _array = tmp._array;
+            _size = tmp._size;
 
-        tmp._array = nullptr;
-        tmp._size = 0;
+            delete[] tmp._array;
+            tmp._array = nullptr;
+            tmp._size = 0;
+        }
+        else {
+            delete[] tmp._array;
+            tmp._array = nullptr;
+            tmp._size = 0;
+        }
     }
-    else {
-        tmp._array = nullptr;
-        tmp._size = 0;
-    }
-    return *this;
-
-    };
 
     ~Vector(){
         delete [] _array;
         _array = nulltr;
-    };
+    }
 
     size_t GetSize() const{
        return _size; 
-    };
+    }
 
     size_t GetStartIndex() const{
         return _startIndex;
-    };
+    }
 
     T& operator[](size_t i){
         return _array[i];
-    };
+    }
 
     T& GetElem(size_t i){
         if (i >= _size){
             throw "Out of range";
         }
         return _array[i];
-    };
+    }
 
     Vector& operator=(const Vector& tmp){
         
-    };
+    }
 
     bool operator==(const Vector& tmp){
         if (_size == tmp._size)
@@ -83,26 +83,28 @@ public:
             if (tmp._array[i] != _array[i]) return 0;
         else return 0;
         return 1;
-    };
+    }
 
     Vector operator+(const T& tmp) const{
         Vector ans(*this);
         for (size_t i = 0; i < _size; i++)
             ans[i] += tmp;
         return ans;
-    };
+    }
+
     Vector operator-(const T& tmp) const{
         Vector ans(*this);
         for (size_t i = 0; i < _size; i++)
             ans[i] -= tmp;
         return ans;
-    };
+    }
+
     Vector operator*(const T& tmp) const{
         Vector ans(*this);
         for (size_t i = 0; i < _size; i++)
             ans[i] *= tmp;
         return ans;
-    };
+    }
 
     Vector operator+(const Vector& tmp) const;
     Vector operator-(const Vector& tmp) const;
